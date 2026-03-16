4cf875b40cb52e6d0aceeea9990b73617cd5da03
ToTxY <stuebsdev@gmail.com>
2026-03-15 19:26:24 -0500

    Adjusted spawn location, added world files

--- src/main/kotlin/events/playerConfiguration.kt
+++ src/main/kotlin/events/playerConfiguration.kt
@@ -11,7 +11,7 @@ class playerConfiguration (private val eventHandler: GlobalEventHandler, private
         eventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
             val player = event.player
             event.spawningInstance = instanceContainer
-            val respawnPosition = Pos(8.5, 34.0, 8.5)
+            val respawnPosition = Pos(-18.5, 34.0, 13.5)
             player.respawnPoint = respawnPosition
             if (player.uuid == UUID.fromString("93e00cfa-893d-46ba-8248-d8fcefc9327e") || player.uuid == UUID.fromString("4f61c127-a842-4eb3-9094-133fefd1a09b") || player.uuid == UUID.fromString("62c45d08-8e26-42b4-baa5-924428e72ce6")) {
                 player.permissionLevel = 4
