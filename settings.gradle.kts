dbc814f4280686a5d23ae23844cdb033e8ac90af
vardins <stuebsdev@gmail.com>
2026-03-17 21:20:56 -0500

    ba boosh

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "abyssNetwork"
+rootProject.name = "slamblock"
--- src/main/kotlin/events/gamemodeSwitcher.kt
+++ src/main/kotlin/events/gamemodeSwitcher.kt
@@ -11,7 +11,7 @@ class gamemodeSwitcher {
         eventHandler.addListener(PlayerGameModeRequestEvent::class.java) {event ->
             if (event.player.permissionLevel >= 3) {
                 event.player.gameMode = event.requestedGameMode
-                event.player.sendMessage(Component.text("AbyssNetwork | Your gamemode has been set to " + (event.requestedGameMode)).color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.BOLD))
+                event.player.sendMessage(Component.text("Abyss | Your gamemode has been set to " + (event.requestedGameMode)).color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.BOLD))
             }
         }
     }
